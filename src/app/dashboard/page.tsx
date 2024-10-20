0d6940be1d661166333ebbb280eb04f1e8950486
GokulSNarayan <gokulnarayan09@gmail.com>
2024-10-20 22:47:02 +0530

    feat: add dashboard and ship page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@/components/ui/typography";
+
+export default async function Dashboard() {
+    return (
+        <div className="p-6">
+            <Typography variant="h2">Welcome to the Ships Dashboard</Typography>
+        </div>
+    )
+}
--- src/app/dashboard/ships/page.tsx
+++ src/app/dashboard/ships/page.tsx
@@ -1,14 +1,14 @@
 import { Typography } from "@/components/ui/typography";
 import { db } from "@/db";
-import { Ship, ship } from "@/db/schema";
-import { DataTable } from "./data-table";
-import { columns } from "./columns";
+import { ship } from "@/db/schema";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
 
 export default async function Ships() {
     const data = await db.select().from(ship);
 
     return (
-        <div className="p-8">
+        <div className="mx-2 p-8">
             <Typography className="mb-2" variant="h3">Ships</Typography>
             <DataTable columns={columns} data={data} />
         </div>
